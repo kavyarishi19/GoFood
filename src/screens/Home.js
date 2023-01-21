069daf11a57c8d48c97d02198b73d83ba8533cd0
kavyarishi19 <forcodingpurposes19@gmail.com>
2023-01-22 01:18:37 +0530

    day2 upgrades

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -5,7 +5,13 @@ export default function Home() {
   return (
     <div>
       <div><Navbar/></div>
-      <div>Body</div>
+      <div><div className="card" style={{"width": "18rem","":""}}>
+        <img className="card-img-top" src="..." alt="Card image cap"/>
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+       </div>
+</div></div>
       <div>Footer</div>
     </div>
   )
