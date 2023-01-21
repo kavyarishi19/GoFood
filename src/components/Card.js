3ea8fd1c1b63cb5b866cb13f3fd3a4397b9c432e
kavyarishi19 <forcodingpurposes19@gmail.com>
2023-01-22 05:02:02 +0530

    some more additions

---
+++ src/components/Card.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+export default function Card() {
+    return (
+        <div>
+
+            <div><div className="card mt-3" style={{ "width": "18rem", "maxHeight": "360px" }}>
+                <img className="card-img-top" src="https://www.shutterstock.com/image-photo/chilli-paneer-dry-made-using-600w-2093440615.jpg" alt="Card image cap" />
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Lorem ipsum dolores</p>
+                    <div className='container w-100'>
+                        <select className='m-2 h-100 bg-success'>
+                            {Array.from(Array(6), (e, i) => {
+                                return (
+                                    <option key={i + 1} value={i + 1} >{i + 1}</option>
+                                )
+                            })}
+                        </select>
+
+                        <select className='m-2 h-100 bg-success rounded'>
+                            <option value="half">Half</option>
+                            <option value="full">Full</option>
+                        </select>
+
+                        <div className='d-inline h-100 fs-5'>
+                            Total Price
+                        </div>
+                    </div>
+                </div>
+            </div></div>
+
+        </div>
+    )
+}
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,18 +1,21 @@
 import React from 'react'
+import Card from '../components/Card'
+import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
+import Carousel from '../components/Carousel'
 
 export default function Home() {
   return (
     <div>
       <div><Navbar/></div>
-      <div><div className="card" style={{"width": "18rem","":""}}>
-        <img className="card-img-top" src="..." alt="Card image cap"/>
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-       </div>
-</div></div>
-      <div>Footer</div>
+      <div><Carousel/></div>
+      <div className='m-3'>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/> 
+      </div>
+      <div><Footer/></div>
     </div>
   )
 }
